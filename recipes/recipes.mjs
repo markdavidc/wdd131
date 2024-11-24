393a024261c0594cee4f5f06be2441f5a4c692d2
markdavidc <davidhawk2001@gmail.com>
2024-11-23 22:50:18 -0700

    more than 1

--- recipes/recipes.mjs
+++ recipes/recipes.mjs
@@ -1,5 +1,59 @@
 const recipes = [
 	{
+		tags: ['Waffles', 'Sweet Potato', 'Side'],
+		description: 'Savory waffles made with Sweet potato with a hint of Ginger',
+		image: './images/sweet-potato-waffle-md.jpg',
+		name: 'Sweet Potato Waffles',
+		rating: 4
+	},
+	{
+		tags: ['Chicken', 'Entree'],
+		description:
+			'Delicious quick and easy creamy rice dish. The mustard, mushrooms, and lemon all blend together wonderfully',
+		image: './images/escalopes-de-poulet-a-la-creme.webp',
+		name: 'Escalope de Poulet a la Creme with steamed green beans (Chicken with Cream)',
+		rating: 4.5
+	},
+	{
+		tags: ['Potatoes', 'side'],
+		description:
+			'Easy and delicious oven roasted potatoes that go great with almost anything.',
+		image: './images/roasted-potatoes.webp',
+		name: 'Oven Roasted potato slices',
+		rating: 4
+	},
+	{
+		tags: ['Southwest', 'entree'],
+		description:
+			'Black beans and tomatoes served over a bed of rice. Top with cheese and scoop up with tortilla chips for maximum enjoyment.',
+		image: './images/black-beans-and-rice.jpg',
+		name: 'Black Beans and Rice',
+		rating: 3
+	},
+	{
+		tags: ['chicken', 'entree', 'Indian'],
+		description:
+			'Quick and easy Chicken curry recipe made with easy to find ingredients.',
+		image: './images/chicken-curry.webp',
+		name: 'Chicken Curry',
+		rating: 5
+	},
+	{
+		tags: ['dessert'],
+		description: 'Delicious soft chocolate chip cookies with coconut.',
+		image: './images/chocolate-chip-cookies.jpg',
+		name: 'Chocolate Chip Cookies',
+		rating: 5
+	},
+	{
+		tags: ['dessert', 'German'],
+		description:
+			"This gooseberry cake with crumble is easy to follow, a bit tart and not too sweet. Made up of a cake base, filled with fresh gooseberries and vanilla cream and finished off with crumble that's flavored with vanilla. A must have recipe for gooseberry lovers!!",
+		image: './images/german-gooseberry-cake.jpg',
+		name: 'Gooseberry cake with vanilla cream and crumble',
+		rating: 5
+	},
+	{
 		tags: ['dessert'],
 		description:
 			"This apple crisp recipe is a simple yet delicious fall dessert that's great served warm with vanilla ice cream.",
